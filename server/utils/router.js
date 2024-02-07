b8b6d670b0ce974e661364ed9faeb942c9848c58
T.Motoyoshi <152137877+VongoleRosso@users.noreply.github.com>
2024-02-07 13:44:01 +0900

    Add files via upload

---
+++ server/utils/router.js
@@ -0,0 +1,94 @@
+import { Router } from "express";
+import {
+  findTrainers,
+  findTrainer,
+  upsertTrainer,
+} from "~/server/utils/trainer";
+import { findPokemon } from "~/server/utils/pokemon";
+
+const router = Router();
+
+router.get("/hello", (_req, res) => {
+  res.send("Hello World");
+});
+
+/** トレーナー名の一覧の取得 */
+router.get("/trainers", async (_req, res, next) => {
+  try {
+    const trainers = await findTrainers();
+    const trainerNames = trainers.map(({ Key }) => Key.replace(/\.json$/, ""));
+    res.send(trainerNames);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** トレーナーの追加 */
+router.post("/trainer", async (req, res, next) => {
+  try {
+    // TODO: リクエストボディにトレーナー名が含まれていなければ400を返す
+    // TODO: すでにトレーナー（S3 オブジェクト）が存在していれば409を返す
+    const result = await upsertTrainer(req.body.name, req.body);
+    res.status(result["$metadata"].httpStatusCode).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** トレーナーの取得 */
+router.get("/trainer/:trainerName", async (req, res, next) => {
+  try {
+    const { trainerName } = req.params;
+    const trainer = await findTrainer(trainerName);
+    res.send(trainer);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** トレーナーの更新 */
+router.post("/trainer/:trainerName", async (req, res, next) => {
+  try {
+    const { trainerName } = req.params;
+    // TODO: トレーナーが存在していなければ404を返す
+    const result = await upsertTrainer(trainerName, req.body);
+    res.status(result["$metadata"].httpStatusCode).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** トレーナーの削除 */
+// TODO: トレーナーを削除する API エンドポイントの実装
+
+/** ポケモンの追加 */
+router.post("/trainer/:trainerName/pokemon", async (req, res, next) => {
+  try {
+    const { trainerName } = req.params;
+    const trainer = await findTrainer(trainerName);
+    if (!("name" in req.body && req.body.name.length > 0))
+      return res.sendStatus(400);
+    const pokemon = await findPokemon(req.body.name);
+    const {
+      order,
+      name,
+      sprites: { front_default },
+    } = pokemon;
+    trainer.pokemons.push({
+      id: (trainer.pokemons[trainer.pokemons.length - 1]?.id ?? 0) + 1,
+      nickname: "",
+      order,
+      name,
+      sprites: { front_default },
+    });
+    const result = await upsertTrainer(trainerName, trainer);
+    res.status(result["$metadata"].httpStatusCode).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** ポケモンの削除 */
+// TODO: ポケモンを削除する API エンドポイントの実装
+
+export default router;
